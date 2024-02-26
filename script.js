fb683bf6753d50c8f0b19537d3d586676cdbc470
temporary <temporary22@gmail.com>
2024-02-26 14:24:44 +0630

    add currentAndtotaltime

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playlistContainerTag = document.getElementsByClassName("playlistContainer")[0];
 
 const audioTag = document.getElementsByClassName("audioTag")[0];
+
+const currentTimeAndDurationTag = document.getElementsByClassName("currentAndTotalTime")[0];
+
 const tracks = [
     {trackId : "music/track1.mp3", title: "December night"},
     {trackId : "music/track2.mp3", title: "Hello world"},
@@ -21,3 +24,25 @@ for (let i = 0;i < tracks.length; i++){
     playlistContainerTag.append(tracksTag);
 
 };
+let durationText = "00:00";
+audioTag.addEventListener("loadeddata", () => {
+    const duration = Math.floor(audioTag.duration); // without milisec
+    durationText = minuteAndSecondText(duration);
+});
+
+audioTag.addEventListener("timeupdate", () => {
+    const currentTime = Math.floor(audioTag.currentTime);
+    const currentText = minuteAndSecondText(currentTime);
+    const currentAndDurationText = currentText + "/" +durationText;
+    currentTimeAndDurationTag.textContent = currentAndDurationText;
+});
+
+const minuteAndSecondText = (totalTime) => {
+    const minutes = Math.floor(totalTime / 60);
+    const seconds = totalTime % 60;
+
+    const minuteText = minutes < 10 ? "0" +minutes.toString() : minutes;
+    const secondsText = seconds < 10 ? "0" +seconds.toString() : seconds;
+    return minuteText + ":" + secondsText;
+};
+
