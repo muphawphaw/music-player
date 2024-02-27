10dede607d75d304284c0972754f45f36390c31a
temporary <temporary22@gmail.com>
2024-02-27 08:48:11 +0630

    addButtons

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const audioTag = document.getElementsByClassName("audioTag")[0];
 
 const currentTimeAndDurationTag = document.getElementsByClassName("currentAndTotalTime")[0];
 
+const currentProgressTag= document.getElementById("currentProgress");
+
+const playButtonTag = document.getElementsByClassName("playButton")[0];
+
+const pauseButtonTag = document.getElementsByClassName("pauseButton")[0];
+
+const previousButtonTag = document.getElementsByClassName("previousButton")[0];
+
+const nextButtonTag = document.getElementsByClassName("nextButton")[0];
 const tracks = [
     {trackId : "music/track1.mp3", title: "December night"},
     {trackId : "music/track2.mp3", title: "Hello world"},
@@ -14,19 +23,20 @@ const tracks = [
 for (let i = 0;i < tracks.length; i++){
     const tracksTag = document.createElement("div");
     tracksTag.addEventListener("click", () =>{
-        const trackId = tracks[i].trackId;
-        audioTag.src = trackId;
-        audioTag.play();
+        currentPlayingIndex = i;
+        playSong();
     })
     tracksTag.classList.add("trackItem");
     const title = (i + 1).toString()+ ". "+ tracks[i].title;
     tracksTag.textContent = title;
     playlistContainerTag.append(tracksTag);
-
+    
 };
+
+let duration = 0;
 let durationText = "00:00";
-audioTag.addEventListener("loadeddata", () => {
-    const duration = Math.floor(audioTag.duration); // without milisec
+audioTag.addEventListener("loadeddata", () => { //fire event
+    duration = Math.floor(audioTag.duration); // without milisec
     durationText = minuteAndSecondText(duration);
 });
 
@@ -35,7 +45,13 @@ audioTag.addEventListener("timeupdate", () => {
     const currentText = minuteAndSecondText(currentTime);
     const currentAndDurationText = currentText + "/" +durationText;
     currentTimeAndDurationTag.textContent = currentAndDurationText;
+    updateCurrentProgress(currentTime);
 });
+
+const updateCurrentProgress = (currentTime) =>{
+   const currentProgressWidth = (500/duration) * currentTime;
+   currentProgressTag.style.width = currentProgressWidth.toString() + "px"; // js to css
+}
 
 const minuteAndSecondText = (totalTime) => {
     const minutes = Math.floor(totalTime / 60);
@@ -46,3 +62,55 @@ const minuteAndSecondText = (totalTime) => {
     return minuteText + ":" + secondsText;
 };
 
+let currentPlayingIndex = 0;
+let isPlaying = false;
+playButtonTag.addEventListener("click", ()=>{
+    const currentTime = Math.floor(audioTag.currenTime);
+    isPlaying= true;
+    if (currentTime === 0){
+        playSong();  
+    }else{
+        audioTag.play();
+        updatePlayAndPauseButton();
+    };
+});
+
+pauseButtonTag.addEventListener("click",() => {
+    isPlaying = false;
+    audioTag.pause();
+    updatePlayAndPauseButton();
+})
+
+previousButtonTag.addEventListener("click", () => {
+    if(currentPlayingIndex === 0){
+        return;
+    }else{
+        currentPlayingIndex -= 1;
+        playSong();
+    }
+});
+
+nextButtonTag.addEventListener("click", () => {
+    if(currentPlayingIndex === tracks.length -1){
+        return;
+    }else{
+        currentPlayingIndex += 1;
+        playSong();
+    }
+})
+const playSong=()=>{
+    const songIdToPlay = tracks[currentPlayingIndex].trackId;
+    audioTag.src = songIdToPlay;
+    audioTag.play();
+    isPlaying= true;
+    updatePlayAndPauseButton();
+}
+const updatePlayAndPauseButton= () => {
+    if(isPlaying){
+        playButtonTag.style.display = "none";
+        pauseButtonTag.style.display = "inline";
+    }else{
+        playButtonTag.style.display = "inline";
+        pauseButtonTag.style.display = "none";
+    }
+}
